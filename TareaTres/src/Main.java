be291c24ddb5e2bc857f2d4dcd9b515e7d692dbe
Jeanca57707 <jean_duarte@ucne.edu.do>
2026-09-14 18:58:59 -0400

    Primer commit

---
+++ TareaTres/src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+
+
+    public static void main(String[] args){
+
+        String [] nombres = {"Juan Perez", "Ana Lopez", "Carlos Ruiz", "Maria DIaz", "Luis Marte"};
+        String[] correos = {"juan@ucne.edu","ana@gmail.com","carlos@ucne.edu","maria@ahoo.com","luis@ucne.edu"};
+
+        double []parcial1 = {8.5,9.0,6.3,4.5,7.8};
+        double []parcial2 = {7.0,8.5,5.9,6.0,9.2};
+        double []parcial3 = {9.0,7.5,4.8,8.0,6.5};
+
+        
+    }
+}
